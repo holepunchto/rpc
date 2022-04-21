6879733a814012fda04c4048056a319fe9ee0a7e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-04-21 14:16:59 +0200

    Improve lifecycle management

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const EventEmitter = require('events')
 const HashMap = require('turbo-hash-map')
 const ProtomuxRPC = require('protomux-rpc')
 
@@ -9,39 +10,79 @@ module.exports = class HyperswarmRPC {
     this._servers = new Set()
   }
 
-  createServer () {
-    const server = new Server(this._dht)
+  createServer (opts) {
+    const server = new Server(this._dht, opts)
+
     this._servers.add(server)
+    server.on('close', () => this._servers.delete(server))
+
     return server
   }
 
-  request (publicKey, method, value, opts = {}) {
+  async request (publicKey, method, value, opts = {}) {
     let rpc = this._connections.get(publicKey)
 
     if (rpc === undefined) {
       const stream = this._dht.connect(publicKey, opts)
 
       rpc = new ProtomuxRPC(stream, { id: publicKey })
+
       this._connections.set(publicKey, rpc)
+      rpc.on('close', () => this._connections.delete(publicKey))
     }
 
     return rpc.request(method, value, opts)
   }
+
+  async destroy (opts = {}) {
+    if (!opts.force) {
+      const closing = []
+
+      for (const server of this._servers) {
+        closing.push(server.close())
+      }
+
+      await Promise.allSettled(closing)
+    }
+
+    for (const rpc of this._connections.values()) {
+      rpc.close()
+    }
+  }
 }
 
-class Server {
-  constructor (dht) {
-    this._dht = dht
+class Server extends EventEmitter {
+  constructor (dht, opts) {
+    super()
 
-    this._server = this._dht.createServer(this._onconnection.bind(this))
+    this._dht = dht
 
     this._connections = new HashMap()
     this._responders = new Map()
+
+    this._server = this._dht.createServer(typeof opts === 'object' && opts)
+    this._server
+      .on('close', this._onclose.bind(this))
+      .on('listening', this._onlistening.bind(this))
+      .on('connection', this._onconnection.bind(this))
+  }
+
+  _onclose () {
+    this._connections.clear()
+    this._responders.clear()
+
+    this.emit('close')
+  }
+
+  _onlistening () {
+    this.emit('listening')
   }
 
   _onconnection (stream) {
     const rpc = new ProtomuxRPC(stream, { id: this.publicKey })
+
     this._connections.set(stream.publicKey, rpc)
+    rpc.on('close', () => this._connections.delete(stream.publicKey))
 
     for (const [method, { opts, fn }] of this._responders) {
       rpc.respond(method, opts, fn)
@@ -52,8 +93,16 @@ class Server {
     return this._server.publicKey
   }
 
+  address () {
+    return this._server.address()
+  }
+
   async listen (keyPair) {
     await this._server.listen(keyPair)
+  }
+
+  async close () {
+    await this._server.close()
   }
 
   respond (method, opts, fn) {
@@ -67,5 +116,7 @@ class Server {
     for (const rpc of this._connections.values()) {
       rpc.respond(method, opts, fn)
     }
+
+    return this
   }
 }
