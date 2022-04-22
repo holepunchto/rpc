0af643be0614895db5f4ad693c382a1286e774b7
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-04-22 14:36:14 +0200

    Add lifecycle tests

--- test.mjs
+++ test.mjs
@@ -1,4 +1,5 @@
 import test from 'brittle'
+import DHT from '@hyperswarm/dht'
 import createTestnet from '@hyperswarm/testnet'
 
 import RPC from './index.js'
@@ -17,4 +18,137 @@ test('basic', async (t) => {
     await rpc.request(server.publicKey, 'echo', Buffer.from('hello world')),
     Buffer.from('hello world')
   )
+})
+
+test('server address', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  t.alike(server.address(), {
+    host: dht.host,
+    port: dht.port,
+    publicKey: server.publicKey
+  })
+})
+
+test('listen using default key pair', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  t.is(server.publicKey, rpc.defaultKeyPair.publicKey)
+})
+
+test('listen using custom default key pair', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+  const keyPair = DHT.keyPair()
+
+  const rpc = new RPC({ dht, keyPair })
+
+  t.is(rpc.defaultKeyPair, keyPair)
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  t.is(server.publicKey, keyPair.publicKey)
+})
+
+test('add responder after connection', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  await t.exception(rpc.request(server.publicKey, 'void'), /unknown method 'void'/)
+
+  server.respond('void', () => {})
+
+  await t.execution(rpc.request(server.publicKey, 'void'))
+})
+
+test('destroy', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  await rpc.destroy()
+
+  t.is(server.closed, true)
+  t.is(dht.destroyed, false)
+})
+
+test('force destroy', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  await rpc.destroy({ force: true })
+
+  t.is(server.closed, false)
+})
+
+test('reject in-progress request on server close', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  server.respond('void', () => {})
+
+  const request = rpc.request(server.publicKey, 'void')
+
+  await server.close()
+
+  t.exception(request, /channel closed/)
+})
+
+test('reject in-progress request on destroy', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  server.respond('void', () => {})
+
+  const request = rpc.request(server.publicKey, 'void')
+
+  await rpc.destroy()
+
+  t.exception(request, /channel closed/)
+})
+
+test('reject in-progress request on force destroy', async (t) => {
+  const [dht] = await createTestnet(3, t.teardown)
+
+  const rpc = new RPC({ dht })
+
+  const server = rpc.createServer()
+  await server.listen()
+
+  server.respond('void', () => {})
+
+  const request = rpc.request(server.publicKey, 'void')
+
+  await rpc.destroy({ force: true })
+
+  t.exception(request, /channel closed/)
 })
